c0a9d61da1e60901a6ad7307a1b29232bdc5e720
EmilyC <EmilyC@JMPERICIA.LOCAL>
2023-03-31 16:25:45 -0300

    2015-2019 cp finished

--- test.py
+++ test.py
@@ -9,7 +9,24 @@ import re
 index_of_pages = 0
 
 while index_of_pages < 56:
-    df = tabula.read_pdf('./test_docs/cp/2015-2019.pdf', pages='all', area=[[122.54,36.28,329.04,259.93]])[index_of_pages]
+
+    if index_of_pages == 4:
+        y_one = 122.54
+        x_one = 36.28
+        y_two = 306
+        x_two = 259.93
+    elif index_of_pages == 0:
+        y_one = 122.54
+        x_one = 36.28
+        y_two = 388
+        x_two = 259.93
+    else:
+        y_one = 122.54
+        x_one = 36.28
+        y_two = 335
+        x_two = 259.93
+
+    df = tabula.read_pdf('./test_docs/cp/2015-2019.pdf', pages='all', area=[[y_one, x_one, y_two, x_two]])[index_of_pages]
     df.to_csv('./tests/2023_03_30-0906/'+str(index_of_pages)+'-edit-test.csv', index=False)
     with open('./tests/2023_03_30-0906/'+str(index_of_pages)+'-edit-test.csv', mode='r+') as file:
         file_readed = file.read()
@@ -25,20 +42,22 @@ while index_of_pages < 56:
         del_single_comma = re.sub(r',á', '', del_special_chars)
         del_special_comma = re.sub(r'á,', ',', del_single_comma)
         del_single_comma_space = re.sub(r'    çã,', '', del_special_comma)
-        date_comma = re.sub(r'(?<=[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9])', ',', del_single_comma_space)
-        del_double_comma = re.sub(r',,', ',',date_comma)
-        del_double_comma_again = re.sub(r',,', ',',del_double_comma)
-        del_hidden = re.sub('    §£,', '', del_double_comma_again)
-        # del_hidden = re.sub(r'/\ufeff+/g', '-', del_double_comma_again)
-        # newStr = del_hidden.replace('\ufeff', '\u000A')
+        del_double_comma = re.sub(r',,', '',del_single_comma_space)
+        put_comma = re.sub(r'(?<=[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9])', ',', del_double_comma)
+        del_commas = re.sub(',,', ',', put_comma)
+        del_hidden = re.sub('§£', '', del_double_comma)
+        put_comma_again = re.sub(r'(?<=[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9])', ',', del_hidden)
+        del_spaces=re.sub(r'\t\n\r', '', put_comma_again)
+        del_whitespaces= re.sub(" ", "", del_spaces)
+        del_double_comma_again = re.sub(',,', ',', del_whitespaces)
 
         csv_file = open('./tests/2023_03_30-0906/all_pages.csv', mode='r+')
         csv_file.read()
-        csv_file.write(del_hidden)
+        csv_file.write(del_double_comma_again)
         csv_file.truncate()
 
         file.seek(0)
-        file.write(del_hidden)
+        file.write(del_double_comma_again)
         file.truncate()
     
     os.remove('./tests/2023_03_30-0906/'+str(index_of_pages)+'-edit-test.csv')
