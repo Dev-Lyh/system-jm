6334c3e6d8268b4213bdccaf180b6d8bc30776d2
EmilyC <EmilyC@JMPERICIA.LOCAL>
2023-04-04 18:23:00 -0300

    Inicio do desenvolvimento da GUI

--- interface.py
+++ interface.py
@@ -1,19 +1,50 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
+from tkinter import *
 
-root = tk.Tk()
-text = tk.Text(root, height=12)
-root.geometry('300x110')
-root.title('Automatizador de Digitação')
-ttk.Label(root, text='Classic Label', foreground="#0066ff", font=('Silkscreen',12)).pack()
-def callback():
-	f = fd.askopenfile()    
-	text.insert('1.0', f.readlines())
-ttk.Button(
-   root, 
-   text="Demo Button", 
-   command=callback
-).pack()
+from tkinter import filedialog
 
-root.mainloop()
+def browseFiles():
+	filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File", filetypes = (("Arquivos PDF","*.pdf*"),	("Todos os Arquivos","*.*")))
+	
+	label_file_explorer.configure(text="Arquivo aberto: "+filename)
+	
+window = Tk()
+
+w = 800 # width for the Tk root
+h = 500 # height for the Tk root
+
+# get screen width and height
+ws = window.winfo_screenwidth() # width of the screen
+hs = window.winfo_screenheight() # height of the screen
+
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+window.title('Sistema de leitura de cartões ponto')
+
+window.config(background = "white", width=800)
+
+frame = Frame(window)
+frame.place(relx=0.5, rely=0.5, anchor='center')
+	
+label_file_explorer = Label(frame,	text = "Arquivo à selecionar", height = 4,fg = "blue")
+button_explore = Button(frame, text = "Procurar arquivo", command = browseFiles)
+
+checkbox_cp_01 = Checkbutton(frame, offvalue="older", text='Modelo Antigo')
+checkbox_cp_02 = Checkbutton(frame, offvalue="newest", text='Modelo Atual')
+
+label_model_ask = Label(frame, text="Selecione o modelo do cartão ponto")
+label_cp_01 = Label(frame, text="Modelo antigo")
+label_cp_02 = Label(frame, text="Modelo atual")
+
+
+label_file_explorer.grid(column = 1, row = 1)
+
+button_explore.grid(column = 1, row = 2)
+
+label_model_ask.grid(column=1, row=3)
+
+checkbox_cp_01.grid(column = 0, row = 4)
+checkbox_cp_02.grid(column = 1, row = 4)
+window.mainloop()
--- script_one.py
+++ script_one.py
@@ -8,7 +8,7 @@ import re
 
 index_of_pages = 50
 
-while index_of_pages < 97:
+while index_of_pages < 55:
 
     y_one = 148.47
     x_one = 49.86
@@ -25,19 +25,55 @@ while index_of_pages < 97:
         del_triple_comma = re.sub(',,,', ',', fix_number_point)
         del_double_comma = re.sub(',,', ',', del_triple_comma)
         fix_month_year = re.sub(r'(?<=[A-Z][A-Z][A-Z]/[0-9][0-9][0-9][0-9],)', '\n', del_double_comma)
-        fix_weekdays_names = re.sub('', '', fix_month_year)
-
-        # print(fix_weekdays_names)
+        fix_weekdays_names = re.sub(r'[A-Z][A-Z][A-Z],', '', fix_month_year)
 
         csv_file = open('./tests/tests_cp_2007/all_pages.csv', mode='r+')
         csv_file.read()
-        csv_file.write(fix_month_year)
+        csv_file.write(fix_weekdays_names)
         csv_file.truncate()
-
-        file.seek(0)
-        file.write(fix_month_year)
-        file.truncate()
     
     os.remove('./tests/tests_cp_2007/'+str(index_of_pages)+'-edit-test.csv')
     
     index_of_pages += 1
+
+with open('./tests/tests_cp_2007/all_pages.csv') as pages_file:
+    month_reader = pages_file.readlines()
+    global index
+    global output
+    index = 1
+    for line in month_reader:
+
+        if re.match(r'[A-Z][A-Z][A-Z]/[0-9][0-9][0-9][0-9]', line):
+            output = line
+
+        # print(str(output) + line)
+        all_date = re.sub(r'(?<=[0-9][0-9]),', '/'+str(output).replace('\n', ''), line)
+        other = re.sub(r'(?<=:[0-9][0-9])', '|', all_date)
+        ot = re.sub(r'[ | ]/[A-Z][A-Z][A-Z]/[0-9][0-9][0-9][0-9]', '', other)
+        pipe = re.sub(r'[ | ]', '', ot)
+        remove_prefix = pipe.removeprefix(',')
+        months = re.sub(r'[A-Z][A-Z][A-Z]/[0-9][0-9][0-9][0-9]/[A-Z][A-Z][A-Z]/[0-9][0-9][0-9][0-9],', '', remove_prefix)
+
+        txt_file = open('./tests/tests_cp_2007/output.csv', mode='r+', encoding='utf-8')
+        txt_file.read()
+        txt_file.write(months)
+        txt_file.truncate()
+        index = index + 1
+
+with open('./tests/tests_cp_2007/output.csv') as out_file:
+# abrir o arquivo CSV de entrada
+    # ler o conteúdo do arquivo CSV
+    reader = out_file.read()
+
+    # criar uma lista de linhas não vazias
+    non_empty_rows = [row for row in reader if any(field.strip() for field in row)]
+
+# abrir o arquivo CSV de saída
+with open('./aba.csv', 'r+', newline='') as output_file:
+
+    # escrever as linhas não vazias no arquivo CSV de saída
+     output_file.readlines()
+     output_file.seek(0)
+     output_file.write(str(non_empty_rows))
+     output_file.truncate()
+
