4527e2b026f0468632e98fae77dbd5aa69ae898d
EmilyFelicio <emilyfelicio03@gmail.com>
2023-04-09 15:51:48 -0300

    Added: refact, marge script one and two, filter file, count of pages

--- interface.py
+++ interface.py
@@ -1,20 +1,8 @@
 import PySimpleGUI as sg
-import script_one as so
-import script_two as st
-import signal
-import time
-from script_one import read_hours
-from script_one import format_dates
 
-gfudai = ''
 class Interface:
-    def __init__(self):
+    def __init__(self, ind, qtd, func1, func2, func3):
 
-        global gfudai
-
-        mds = 0
-        total_pages = ''
-        
         radio_unchecked = b'iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAEwElEQVR4nI1W3W9URRT/nZm7ZXdpbajdWpCAjcFEqw88+CACrgaBmFBIwI3fPPpPaJYND/wjYsxFYgwP+BV2kY9gNCIJIhEIBZSWLl3aprvde2fOOT7c3W27fNSTTO7cMzO/35wz55wZYAVRVVMuaxCGoV2qD8PQlsvlQFXNShhPAqduYEr0lrrmhmFoVbVbvWzdQxKGoS0UCgwAFy6PvySx27cQRVvY80YGZyHaIKJbPUHqvCF8k3/tlb+61z2RJAzVFgrE5QuX1q9K9x6Oouj9TCazKmUBawiAglkQO0bsPOqNejOw9qsoan62Z8eWfx9FRMsJkgnnfrv6FgXBUWOD4UzAWJsb8L3ZNFlrCQSwZ8TO6excXe/eux/UY0EcuQkXRx/t3fX6qW6iDomqGiKS87///QaM/Q7K6efXD7rBgf5AVcl7hgBQEYgqVAQEgqroZLXmb9yeTLGgKRztHtu5/XQbr0NSVDU4dAhvj703LGouBpaGXhwZ5v6nem0cO2gCB002AxGBiICZwSwIrEVtZpav3LhjneN76YxsvnDq1D0AKJVKYgBg9NgxKpVKIkpH0ulVQyPrBvxTfb02ih2ICESAdp2darJHIkIUx+jrXW03rB30PT09zzTm5UipVJLR0VECAGqb9csfV16oN3H56f60Hd20gZzzRJR4UzvAusySxBoBi8A5DyLolWvjOv1gjldnUqN7duavFYtFYyoVGACIvd2fzWZSw4P9IqKkLfBugu4GKFSSr4hSbqBfMplMaiFyBwAgn88bU60eUwCI43hbYIBsJk2e+bHAiQVL/xWiSTB4ZmQzabKG4B1vBYBqtapBoVBgVaUfz13aaI3CEBGzgAjouEuXg3bARSG6pImADJEhwLN/TlWJiDhoecOqSHYpUIJPHYclY4CqdBElZ6Otfse9otlKBRaAb5OwqjbaYSnatqKzpEXQAleFsIAlCWERBbfyR4TBwlDVRj4PBgAThqElIgVhPPaicew02R0vi6ClESWcALEkkbV0bhQ7dZ4VpONEpGEYWpPL5QgArLVnYsc0N99QAuC5nWy8JPEYvtW4PS6LfVXFfL2hznkyxv4MALlcjkwlnxcACCj4ul6fjyeqNeOZ1Xu/COoXwX0XkbDAs8B7BjPrVLVm6vVGDOXjAFCpVMSUiCQMQ/vmlpevE+nRyJOZul9jYwix84sEfrG1d94h9A5EQHW6xrEXYwhffFLYe/3dMLSlUkmS2lUsGgB4Nf/OEIleJEPDI88Ocl/vauu8b5UQdA69nS/t2mWIMDM3x+P/TFp2flKM3Tz+569T7dr1UBU+8dPZbWRS30M4s25ojVvT3xcIlNpRpCpd+cI6XZvxd6emUyrUEPW7DhbGzi6twp37mVpu27Nj65lmo7lbgDsT9+dSV2/cotqDWR/HMYt4ERHx7CWKIq7NzPrrN2/TVG0uBcVt56PdBwtjZ1sRKx3sruLaubiOnzy51tq+wy6KP0j19GSsAQwtlnrPjNgxmgvNBWvNl41m8/NPP94/seLN2E0EACd+qGxyse5runi7Zz+iLL2imLcGN1PWnhYNvv3wwM5r3ev+lzzqtdLSB926lV4rK0qxWDTlcvmx7652ZD5J/gNoDCDS80MCGwAAAABJRU5ErkJggg=='
 
         radio_checked = b'iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAF40lEQVR4nI2Wf2yWVxXHv+fe+7y/3xbYWvpzhbGRCOkMLoRsjr21A2dI2BalTeaYxsyQ6GT+YTQuQRsy4zRGtmg2gzGNf+jinoK6sY2ZbNK3JQuSuWmiWx3ggBQKfTta+v58nueee/zjfQusMPD88yT3ued87sk593sPcCMTUblDYgZ80R9b90XnDomBiLphjOsEp8WBNQEiohUt2uuLhsji1Ut2zR8Dvq9HBgcZAPqPzK+ZD81DxWpwt2XucYIURCqa6FQmHnuryeBPY31N79dhvkbD77qQAV/0yCBx7tBMV0knn5oPooczyVR8Rcyi0zAS5FBhYDLQ+DDUKJWrtaxRf0hF87uObL3lzIL/J0IWNmx8c7Z/zsR/b7Rp25qex7aOuL09ayhhiECAs4xSyPLBxVD2T4bmQLkZURRNZaLi9nce7P4rfNG4AnQZIqJA5O4Zu5Cbk+TrHVRL/Hi1ie5cnjBgosAyWAAnAnEOEIcYCbRjOXy+an94XHlTHK8tcZUvvP1AR34h3mXIUL1DNm2eaTsXxN5t96R1uNdw15KkrgQMAqAgEAAiAuccnHOI2MFah4wWHJ+t8OMTWp8L9fn2uKwbP9JyHgCwm5wCgIG1IOwmdyH0no4lkq0/uQ22qzmhyzWGIUARINfqEBF4GrBaY83NKb2rJ7Amnlg+U+GnsZvcwNoRqmfSSOu+sYurT1Xdv7a3Oj10R5bKoZAhwAlAtBBTLmViLcMoQhBZfH84j7vXduLhDT3yvX+U5Y8fJXlVMlo7trX7GIZEqdwoFADMMn0pm057X2w3zjkQpH76mFFwTi4BRASWHYxWYCfY+dwb+M3L7+Bn/lHMViN6YDlcOpnwpgO1DQByfVAqXxgRACgHduMKz2JVxlBgHTxNIABnZopIJQwsuwaAYTTBOYcdzx7Ei2MT6O5Yih999bOA1rglAer2IpQZ9wBAvjAiCoODLCJkWXo6TIS4EoqsAwB899dv4q4nfouxf55GNh1HLYhgVD2zHc++jn2HP0D7sjR++c1+3PfpbhSrIZIa1KZCWJYVIkIYHOQF3dFOJJWAA4mAnQOzxdRHRZwtFPGVn76MN94+gZuWphBGFjueOYiR8f+gY1kGzz++CZ+7owuFi5X6nRBBHAxxkhodhQYA04AwQSoVJkTMcE7BMjD8nS0gIuwbn8BjP38Nz+3cjJH8BF7MT6Dz5gye37kJud5OFObKUASwc4gco+o8CFDp6wPXIb6viYhXv3rh5GSkP1UKQ1EaCEJG3NPY++374UTw0lvH8PU9B1GuRWi/KYNffWsz+no7MT1XgSLUa+YcSiHLmcgTD+FJIhL4vla5lgECgFQM4ycDQ8fmI/EgcCKoBhEIgr1PfB4P3nUbpueqaE7HsbeRwfRcGYoEzK7eEMI4XmSZjGKU8PQYAORaBsjkR+EAoNmofadL5d37zrLpbYoktEQeESq1EDFP4xff6Ec26WHL+pVXANAAOITWIUaRvFrQqlyphh0x3g8A+VE4ulIYe18pDLtE+mt72gt2Q0vCzIYCTwHOCYgIqbhBEFlUamG9kA15qVlGRjkcLQR21/kuo2rl4ROPdD+GAV9jZJA/pl259dOtU2LebTW27Zlbq7yyKabnQqnfTAiY619qACzX9SujGP+9GPCTp5bogjXnsiZc996/V0wvaNdVKvyZA2c2zqv0X1pRSz7ZVYnWL9UmFKKABdbVayUigGMYOChn5egM2z3nmr2CJCtZW73/vUd6Dl+twgvWeAfW/fn0vSXd9DttdHe/nsaWFmdXJkEJJUQQROxQDllOlEVeK2gzatvAbE+ng+L29x9dNf7J70nDFupz5/6T7dVY9qli6L6ciMWSXSZAOwWIE6PKhLM2jknroVwNqxmPXlgSXPjB3x9dM7UYcE1IPaPLb/WGA9O3zzM9VAr5XhvZlQ6SIaGSUfRh0jP5ZRS+9Ldt3ccW+/1/JkJYNK0oAg6JmKtmIN+/7rRyYxuqz12LgfD9+tw1dOO563+8H1VJkK2keQAAAABJRU5ErkJggg=='
@@ -34,8 +22,8 @@ class Interface:
         sg.theme('DarkBlue14')
 
         layout = [
-            [sg.Text('Sistema de leitura de cartão ponto', expand_x=True,
-                     justification='center', font=('Calibri Bold', 18))],
+            [sg.Text('Sistema de leitura de cartões ponto', expand_x=True,
+                    justification='center', font=('Calibri Bold', 18))],
             [sg.Graph(
                 canvas_size=(150, 60),
                 graph_bottom_left=(0, 0),
@@ -50,55 +38,36 @@ class Interface:
                 sg.Image(radio_unchecked, enable_events=True, k='2015', metadata=False), sg.T('Modelo atual', enable_events=True, k='-T2-', font=('Calibri', 16))],
 
             [sg.FileBrowse('Procurar arquivo', file_types=(("Arquivos PDF", '*.pdf')), size=(17, 0), font=('Calibri', 14), button_color='#33688A', pad=(0, 10)),
-             sg.Input(key='path', pad=(20, 10), font=('Calibri', 14), border_width=2)],
+            sg.Input(key='path', pad=(20, 10), font=('Calibri', 14), border_width=2)],
 
             [sg.FolderBrowse('Pasta de destino', size=(17, 0), font=('Calibri', 14), button_color='#33688A', pad=(0, 10)),
-             sg.Input(key='folder_path', pad=(20, 10), font=('Calibri', 14), border_width=2)],
+            sg.Input(key='folder_path', pad=(20, 10), font=('Calibri', 14), border_width=2)],
 
             [sg.Text('Quantidade de páginas', font=('Calibri', 14), pad=(0, 10)),
-             sg.Input(key='pages', font=('Calibri', 14), pad=(14, 10), border_width=2)],
-
+            sg.Input(key='pages', font=('Calibri', 14), pad=(14, 10), border_width=2)],
             [sg.Button('Começar', font=('Calibri', 14), pad=(
                 0, 20), expand_x=True, button_color='#0C73B3')],
-            [sg.Text(f'{gfudai}', k='indicator')],
-            [sg.Text('V 1.0.0', font=('Consolas', 8),
-                     justification='center', expand_x=True)]
+            [sg.Text('V 1.0.2', font=('Consolas', 8),
+                    justification='center', expand_x=True)]
         ]
 
-        window = sg.Window(
-            'Sistema de leitura de cartões ponto').layout(layout)
-                
+        window = sg.Window('Sistema de leitura de cartões ponto').layout(layout)
 
-        def FirstModel(pg,pt,fp, inst):
-
-            nonlocal mds
-            mds += 1
-            # so.nf()
-
-
-
-        
         while True:
 
             event, values = window.Read()
 
             path = values['path']
             pages = values['pages']
+            index = ind
             folder_path = values['folder_path']
             radi1 = window['2007'].metadata
             radi2 = window['2015'].metadata
-            indicator = window['indicator']
 
             if event in radio_keys:
                 check_radio(event)
-            if event == 'Começar' and window['2007'].metadata == True:
-                read_hours(pages, path, folder_path)
-                format_dates(folder_path)
-            elif event == 'Começar' and window['2015'].metadata == True:
-                print('Modelo de 2015')
-
-            print(
-                f'Model: {radi1}-{radi2}\r\nPath: {path}\r\nPages quantity: {pages}')
-
-
-Interface()
+            if event == 'Começar' and radi1 == True:
+                func1(pages, path, folder_path)
+                func2(folder_path)
+            elif event == 'Começar' and radi2 == True:
+                func3(pages, path, folder_path)
